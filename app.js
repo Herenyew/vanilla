2b8ccb013ee77e4c0bd01e5b70dc1089bd5a1a56
Herenyew <155099708+Herenyew@users.noreply.github.com>
2024-01-02 14:58:45 +0400

    added css functions
    one function to get the weather value for each city
    and the other to update the time the temperature every time city name is submitted

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ function search(event) {
   let hour = now.getHours();
   let minute = now.getMinutes();
   current_time.innerHTML = `${day} ${hour}:${minute}`;
+  let key = `te40ea2d53204b6fa4eb4fo881b4b3b4`;
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+  axios.get(apiurl).then(get_weather);
 }
 let form = document.querySelector(".search-form");
 form.addEventListener("submit", search);
+
+function get_weather(response) {
+  let temperature_value = document.querySelector("#temp-value");
+  temperature_value.innerHTML = response.data.temperature.current;
+}
