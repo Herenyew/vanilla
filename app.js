2b29204e4c62000e5ba9c6d095719a378c6041f0
Herenyew <155099708+Herenyew@users.noreply.github.com>
2024-01-02 22:21:26 +0400

    added a new function to show the temperature icon for each city

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ function search(event) {
   let h1 = document.querySelector("h1");
   let city = document.querySelector(".search-form-input").value;
   h1.innerHTML = city;
+  let now = new Date();
+  get_date(now);
+  let key = `te40ea2d53204b6fa4eb4fo881b4b3b4`;
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+
+  axios.get(apiurl).then(get_weather);
+  axios.get(apiurl).then(get_weather_condition);
+  axios.get(apiurl).then(get_humidity);
+  axios.get(apiurl).then(get_wind);
+  axios.get(apiurl).then(get_icon);
+}
+
+function get_date(date) {
   let week = [
     "Sunday",
     "Monday",
@@ -17,14 +30,11 @@ function search(event) {
   let day = week[now.getDay()];
   let hour = now.getHours();
   let minute = now.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
   current_time.innerHTML = `${day} ${hour}:${minute}`;
-  let key = `te40ea2d53204b6fa4eb4fo881b4b3b4`;
-  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
-  axios.get(apiurl).then(get_weather);
-  axios.get(apiurl).then(get_weather_condition);
 }
-let form = document.querySelector(".search-form");
-form.addEventListener("submit", search);
 
 function get_weather(response) {
   let temperature_value = document.querySelector("#temp-value");
@@ -36,9 +46,15 @@ function get_weather_condition(response) {
 }
 function get_humidity(response) {
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.temperature.humidity;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
 }
 function get_wind(response) {
   let wind = document.querySelector("#wind");
   wind.innerHTML = response.data.wind.speed;
 }
+function get_icon(response) {
+  let iconElement = document.querySelector(".icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" id="icon" />`;
+}
+let form = document.querySelector(".search-form");
+form.addEventListener("submit", search);
